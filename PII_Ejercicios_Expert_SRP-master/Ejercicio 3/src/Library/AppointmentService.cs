77c161a8cacd0521fce032df60ed553d9a741d76
nicolasvelazquez25 <nicolasvelazquezperez@gmail.com>
2020-09-01 20:55:51 -0300

    Comentarios acerca del ejercicio 3

--- PII_Ejercicios_Expert_SRP-master/Ejercicio 3/src/Library/AppointmentService.cs
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 3/src/Library/AppointmentService.cs
@@ -10,38 +10,38 @@ namespace Library
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
             Boolean isValid = true;
 
-            if (string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name))//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Unable to schedule appointment, Name is required\n");
                 isValid = false;
             }
 
-            if (string.IsNullOrEmpty(id))
+            if (string.IsNullOrEmpty(id))//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Unable to schedule appointment, id is required\n");
                 isValid = false;
             }
 
-            if (string.IsNullOrEmpty(phoneNumber))
+            if (string.IsNullOrEmpty(phoneNumber))//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
                 isValid = false;
             }
 
-            if (string.IsNullOrEmpty(appoinmentPlace))
+            if (string.IsNullOrEmpty(appoinmentPlace))//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Unable to schedule appointment, Appoinment place is required\n");
                 isValid = false;
             }
 
             
-            if (string.IsNullOrEmpty(doctorName))
+            if (string.IsNullOrEmpty(doctorName))//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
                 isValid = false;
             }
 
-            if (isValid)
+            if (isValid)//Es posible crear una funcion que haga esta revision y de ser necesario devuelva un string.
             {
                 stringBuilder.Append("Appoinment Scheduled");
             }
@@ -51,3 +51,11 @@ namespace Library
 
     }
 }
+/*
+
+es necesario crear otros metodos para verificar cada uno de los datos ingresados de manera que sea posible simplificar este codigo, de este modo, si existe un error, 
+este sera mas facil de identificar y solucionar (con su clase propia)
+
+tambien es necesario crear un metodo que sea capaz de crear la cadena a devolver, haciendo mas facil la identifiacion de problemas en el producto final
+
+*/
