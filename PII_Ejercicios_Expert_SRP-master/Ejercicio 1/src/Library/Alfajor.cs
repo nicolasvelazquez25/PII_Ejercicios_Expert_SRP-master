36967fc89e0dc1d98bf1e8cfdde275f7d2aeb052
nicolasvelazquez25 <69222891+nicolasvelazquez25@users.noreply.github.com>
2020-09-01 19:53:15 -0300

    Update Alfajor.cs
    Corrección info. confusa

--- PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Alfajor.cs
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Alfajor.cs
@@ -4,7 +4,7 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
-        public Alfajor(double precioMasa, double precioDulce)// En este caso esta funcion esta bien aplicada ya que ella misma se resuelve su precio sin ser demasiado compleja, igualmente es posible crear otro calculador de precios.
+        public Alfajor(double precioMasa, double precioDulce)// En este caso esta funcion esta bien aplicada ya que ella misma se resuelve su precio sin ser demasiado compleja, Aunque el mismo deberia calcularse su propio precio, ya que el posee toda la informacion necesaria.
         {
             this.PrecioDulce = precioDulce;
             this.PrecioMasa = precioMasa;
